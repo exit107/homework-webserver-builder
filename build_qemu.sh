4fd284d5f120a8640c7a9056046e9062f320f5e2
Dave Brown <28700674+exit107@users.noreply.github.com>
2020-03-11 14:42:32 -0600

    Reformatted QEMU build script to better match the others.

--- build_qemu.sh
+++ build_qemu.sh
@@ -1,2 +1,15 @@
 #!/usr/bin/bash
-PACKER_LOG=1 PACKER_LOG_PATH="./logs/qemu_build_$(date +%Y%m%d.%H%M%S).log" packer build -timestamp-ui -force packer/webserver-ubuntu-qemu.json
+########################################################
+## Build a local QEMU server from the packer template ##
+########################################################
+# Make the output directory
+if [ ! -d ./output/qemu/ ]; then
+  mkdir -p output/qemu/
+fi
+
+# Logging
+PACKER_LOG=1
+PACKER_LOG_PATH="./logs/qemu_build_$(date +%Y%m%d.%H%M%S).log"
+
+# Build It!
+packer build -timestamp-ui -force packer/webserver-ubuntu-qemu.json
