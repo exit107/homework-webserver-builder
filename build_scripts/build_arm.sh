e6bab413d048bd3276f7235c76c7ebb1efdabe7a
Dave Brown <28700674+exit107@users.noreply.github.com>
2020-03-16 18:23:45 -0600

    Moved sensitive variables to a var file/env vars to facilitate automated builds.

--- build_scripts/build_arm.sh
+++ build_scripts/build_arm.sh
@@ -3,14 +3,9 @@
 ## Build an Azure ARM server from the packer template ##
 ########################################################
 
-# Make the output directory
-if [ ! -d ./output/azure/ ]; then
-  mkdir -p output/azure/
-fi
-
 # Logging
 PACKER_LOG=1
 PACKER_LOG_PATH="./logs/azure_build_$(date +%Y%m%d.%H%M%S).log"
 
 # Build It!
-packer build -timestamp-ui -force packer/webserver-ubuntu-azure.json
+packer build -timestamp-ui -force -vars-file=./vars/azure.json ./packer/webserver-ubuntu-azure.json
