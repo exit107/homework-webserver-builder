1fdeb0437d882eb3fd2641424836dcfc7f6b694d
Dave Brown <28700674+exit107@users.noreply.github.com>
2020-03-18 20:05:59 -0600

    Moved to git ref hash for tagging. All builds (excepting QEMU) natively tag builds with the reference hash for the build.

--- build_scripts/azure-arm.sh
+++ build_scripts/azure-arm.sh
@@ -1,11 +1,25 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
+
 ########################################################
 ## Build an Azure ARM server from the packer template ##
 ########################################################
 
+# Envvars
+export BUILD_VERSION=$(git rev-parse --short HEAD)
+
 # Logging
-PACKER_LOG=1
-PACKER_LOG_PATH="./logs/azure-arm_build_$(date +%Y%m%d.%H%M%S).log"
+export PACKER_LOG=1
+export PACKER_LOG_PATH="./logs/azure-arm_${BUILD_VERSION}_$(date +%Y-%m-%d.%H%M%S).log"
+
+# Notify the build user
+echo "Build starting"
+echo "Logs available at: ${PACKER_LOG_PATH}"
 
 # Build It!
-packer build -timestamp-ui -force -var-file=./vars/SECRETS_azure-arm.json -var-file=./vars/azure-arm.json ./packer/azure-arm.json
+packer build \
+  -timestamp-ui \
+  -force \
+  -var-file=./vars/generic.json \
+  -var-file=./vars/SECRETS_azure-arm.json \
+  -var-file=./vars/azure-arm.json \
+  ./packer/azure-arm.json
